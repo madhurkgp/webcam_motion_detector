e11232c1b43d6879aed869ee5bc8dfc9e313dce6
madhurkgp <madhur.yadav7@gmail.com>
2019-05-08 22:42:45 +0530

    made changes in the code to resolve error , earlier i was calling the gaussian blur func on gray image but now im calling it on image objet cv2, also changed the naming convention as two variables were named same and was causing confusuion

--- motion_detector.py
+++ motion_detector.py
@@ -2,7 +2,7 @@ import cv2,time
 import pandas as pd
 from datetime import datetime
 video = cv2.VideoCapture(0)
-frame = None
+first_frame = None
 status_list = [None,None]
 times=[]
 df = pd.DataFrame(columns=['Start','End'])
@@ -10,14 +10,14 @@ while True:
     status = 0
     check, frame = video.read()
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    gray.GaussianBlur(gray,(21,21),0)
-    cv2.imshow('capturing',gray)
-    if(frame is None):
+    gray= cv2.GaussianBlur(gray,(21,21),0)
+
+    if(first_frame is None):
         first_frame = gray
         continue
     delta_frame = cv2.absdiff(first_frame,gray)
     threshold_frame = cv2.threshold(delta_frame,30,255,cv2.THRESH_BINARY)[1]
-    threshold_frame = cv2.dilate(threshold_frame,None,iterations=5)
+    threshold_frame = cv2.dilate(threshold_frame,None,iterations=10)
     (_,cnts,_) = cv2.findContours(threshold_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     for contour in cnts:
         if cv2.contourArea(contour)<10000:
@@ -35,6 +35,7 @@ while True:
         if status ==1:
             times.append(datetime.now())
         break
+    cv2.imshow('capturing', gray)
     cv2.imshow('Gray Frame',gray)
     cv2.imshow('Delta Frame',delta_frame)
     cv2.imshow('Threshold Frame',threshold_frame)
